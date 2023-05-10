b05a90695afad2d42e2bb93e7d55b19a311e1629
IHo-Chiu <r11922189@ntu.edu.com>
2023-05-10 02:39:21 +0000

    .

--- HW7/main.py
+++ HW7/main.py
@@ -97,7 +97,7 @@ def same_seeds(seed):
 	random.seed(seed)
 	torch.backends.cudnn.benchmark = False
 	torch.backends.cudnn.deterministic = True
-same_seeds(2)
+same_seeds(11922189)
 
 """## Load Model and Tokenizer
 
@@ -106,6 +106,21 @@ same_seeds(2)
 
  
 """
+
+# hyperparameters
+do_train = False
+num_epoch = 2
+validation = False
+logging_step = 100
+learning_rate = 1e-5
+train_batch_size = 8
+doc_stride = 32
+
+#### TODO: gradient_accumulation (optional)####
+# Note: train_batch_size * gradient_accumulation_steps = effective batch size
+# If CUDA out of memory, you can make train_batch_size lower and gradient_accumulation_steps upper
+# Doc: https://huggingface.co/docs/accelerate/usage_guides/gradient_accumulation
+gradient_accumulation_steps = 8
 
 from transformers import (
   AutoTokenizer,
@@ -173,7 +188,7 @@ class QA_Dataset(Dataset):
         self.max_paragraph_len = 150
         
         ##### TODO: Change value of doc_stride #####
-        self.doc_stride = 1
+        self.doc_stride = doc_stride
 
         # Input sequence length = [CLS] + question + [SEP] + paragraph + [SEP]
         self.max_seq_len = 1 + self.max_question_len + 1 + self.max_paragraph_len + 1
@@ -246,12 +261,13 @@ class QA_Dataset(Dataset):
         return input_ids, token_type_ids, attention_mask
 
 train_set = QA_Dataset("train", train_questions, train_questions_tokenized, train_paragraphs_tokenized)
-dev_set = QA_Dataset("dev", dev_questions, dev_questions_tokenized, dev_paragraphs_tokenized)
+dev_set = QA_Dataset("train", dev_questions, dev_questions_tokenized, dev_paragraphs_tokenized)
 test_set = QA_Dataset("test", test_questions, test_questions_tokenized, test_paragraphs_tokenized)
+train_set = torch.utils.data.ConcatDataset([train_set, dev_set])
 
 """## Function for Evaluation"""
 
-def evaluate(data, output):
+def evaluate(data, output, doc_stride=doc_stride, token_type_ids=None, paragraph=None, paragraph_tokenized=None):
     ##### TODO: Postprocessing #####
     # There is a bug and room for improvement in postprocessing 
     # Hint: Open your prediction file to see what is wrong 
@@ -265,7 +281,11 @@ def evaluate(data, output):
         start_prob, start_index = torch.max(output.start_logits[k], dim=0)
         end_prob, end_index = torch.max(output.end_logits[k], dim=0)
         
-        if end_index < start_index:
+        token_type_id = data[1][0][k].detach().cpu().numpy()
+        paragraph_start = token_type_id.argmax()
+        paragraph_end = len(token_type_id) - 1 - token_type_id[::-1].argmax()-1
+        
+        if end_index < start_index or start_index < paragraph_start or end_index > paragraph_end:
             continue
         
         # Probability of answer is calculated as sum of start_prob and end_prob
@@ -276,6 +296,13 @@ def evaluate(data, output):
             max_prob = prob
             # Convert tokens to chars (e.g. [1920, 7032] --> "大 金")
             answer = tokenizer.decode(data[0][0][k][start_index : end_index + 1])
+            origin_start = start_index + k * doc_stride - paragraph_start
+            origin_end = end_index + k * doc_stride - paragraph_start;
+            
+    if '[UNK]' in answer:
+        raw_start =  paragraph_tokenized.token_to_chars(origin_start)[0]
+        raw_end = paragraph_tokenized.token_to_chars(origin_end)[1]
+        answer = paragraph[raw_start:raw_end]
     
     # Remove spaces in answer (e.g. "大 金" --> "大金")
     return answer.replace(' ','')
@@ -284,20 +311,6 @@ def evaluate(data, output):
 
 from accelerate import Accelerator
 
-# hyperparameters
-num_epoch = 1
-validation = True
-logging_step = 100
-learning_rate = 1e-5
-optimizer = AdamW(model.parameters(), lr=learning_rate)
-train_batch_size = 8
-
-#### TODO: gradient_accumulation (optional)####
-# Note: train_batch_size * gradient_accumulation_steps = effective batch size
-# If CUDA out of memory, you can make train_batch_size lower and gradient_accumulation_steps upper
-# Doc: https://huggingface.co/docs/accelerate/usage_guides/gradient_accumulation
-gradient_accumulation_steps = 8
-
 # dataloader
 # Note: Do NOT change batch size of dev_loader / test_loader !
 # Although batch size=1, it is actually a batch consisting of several windows from the same QA pair
@@ -305,6 +318,9 @@ train_loader = DataLoader(train_set, batch_size=train_batch_size, shuffle=True, 
 dev_loader = DataLoader(dev_set, batch_size=1, shuffle=False, pin_memory=True)
 test_loader = DataLoader(test_set, batch_size=1, shuffle=False, pin_memory=True)
 
+print(len(train_loader))
+
+optimizer = AdamW(model.parameters(), lr=learning_rate)
 scheduler = get_cosine_with_hard_restarts_schedule_with_warmup(
     optimizer, num_warmup_steps=100, num_training_steps=num_epoch*len(train_loader))
 
@@ -313,87 +329,89 @@ scheduler = get_cosine_with_hard_restarts_schedule_with_warmup(
 # precision training (fp16)	
 fp16_training = True
 if fp16_training:    
-    accelerator = Accelerator(mixed_precision="fp16")
+    accelerator = Accelerator(mixed_precision="fp16", gradient_accumulation_steps=gradient_accumulation_steps)
 else:
-    accelerator = Accelerator()
+    accelerator = Accelerator(gradient_accumulation_steps=gradient_accumulation_steps)
 
 # Documentation for the toolkit:  https://huggingface.co/docs/accelerate/
 model, optimizer, train_loader = accelerator.prepare(model, optimizer, train_loader) 
 
-model.train()
+if do_train:
+    model.train()
 
 
-print("Start Training ...")
+    print("Start Training ...")
 
-for epoch in range(num_epoch):
-    step = 1
-    train_loss = train_acc = 0
-    
-    for data in tqdm(train_loader):	
-        # Load all data into GPU
-        data = [i.to(device) for i in data]
-        
-        # Model inputs: input_ids, token_type_ids, attention_mask, start_positions, end_positions (Note: only "input_ids" is mandatory)
-        # Model outputs: start_logits, end_logits, loss (return when start_positions/end_positions are provided)  
-        output = model(input_ids=data[0], token_type_ids=data[1], attention_mask=data[2], start_positions=data[3], end_positions=data[4])
-        # Choose the most probable start position / end position
-        start_index = torch.argmax(output.start_logits, dim=1)
-        end_index = torch.argmax(output.end_logits, dim=1)
-        
-        # Prediction is correct only if both start_index and end_index are correct
-        train_acc += ((start_index == data[3]) & (end_index == data[4])).float().mean()
-           
-        train_loss += output.loss
-        
-        accelerator.backward(output.loss)
-        
-        step += 1
-        optimizer.step()
-        optimizer.zero_grad()
-        
-        ##### TODO: Apply linear learning rate decay #####
-        scheduler.step()
+    for epoch in range(num_epoch):
+        step = 1
+        train_loss = train_acc = 0
 
-        # Print training loss and accuracy over past logging step
-        if step % logging_step == 0:
-            print(f"Epoch {epoch + 1} | Step {step} | loss = {train_loss.item() / logging_step:.3f}, acc = {train_acc / logging_step:.3f}")
-            train_loss = train_acc = 0
+        for data in tqdm(train_loader):	
+            # Load all data into GPU
+            data = [i.to(device) for i in data]
 
-    if validation:
-        print("Evaluating Dev Set ...")
-        model.eval()
-        with torch.no_grad():
-            dev_acc = 0
-            for i, data in enumerate(tqdm(dev_loader)):
-                output = model(input_ids=data[0].squeeze(dim=0).to(device), token_type_ids=data[1].squeeze(dim=0).to(device),
-                       attention_mask=data[2].squeeze(dim=0).to(device))
-                # prediction is correct only if answer text exactly matches
-                dev_acc += evaluate(data, output) == dev_questions[i]["answer_text"]
-            print(f"Validation | Epoch {epoch + 1} | acc = {dev_acc / len(dev_loader):.3f}")
-        model.train()
+            # Model inputs: input_ids, token_type_ids, attention_mask, start_positions, end_positions (Note: only "input_ids" is mandatory)
+            # Model outputs: start_logits, end_logits, loss (return when start_positions/end_positions are provided)  
+            output = model(input_ids=data[0], token_type_ids=data[1], attention_mask=data[2], start_positions=data[3], end_positions=data[4])
+            # Choose the most probable start position / end position
+            start_index = torch.argmax(output.start_logits, dim=1)
+            end_index = torch.argmax(output.end_logits, dim=1)
 
-# Save a model and its configuration file to the directory 「saved_model」 
-# i.e. there are two files under the direcory 「saved_model」: 「pytorch_model.bin」 and 「config.json」
-# Saved model can be re-loaded using 「model = BertForQuestionAnswering.from_pretrained("saved_model")」
-print("Saving Model ...")
-model_save_dir = "saved_model" 
-model.save_pretrained(model_save_dir)
+            # Prediction is correct only if both start_index and end_index are correct
+            train_acc += ((start_index == data[3]) & (end_index == data[4])).float().mean()
+
+            train_loss += output.loss
+
+            accelerator.backward(output.loss)
+
+            step += 1
+            optimizer.step()
+            optimizer.zero_grad()
+
+            ##### TODO: Apply linear learning rate decay #####
+            scheduler.step()
+
+            # Print training loss and accuracy over past logging step
+            if step % logging_step == 0:
+                print(f"Epoch {epoch + 1} | Step {step} | loss = {train_loss.item() / logging_step:.3f}, acc = {train_acc / logging_step:.3f}")
+                train_loss = train_acc = 0
+
+        if validation:
+            print("Evaluating Dev Set ...")
+            model.eval()
+            with torch.no_grad():
+                dev_acc = 0
+                for i, data in enumerate(tqdm(dev_loader)):
+                    output = model(input_ids=data[0].squeeze(dim=0).to(device), token_type_ids=data[1].squeeze(dim=0).to(device),
+                           attention_mask=data[2].squeeze(dim=0).to(device))
+                    # prediction is correct only if answer text exactly matches
+                    dev_acc += evaluate(data, output) == dev_questions[i]["answer_text"]
+                print(f"Validation | Epoch {epoch + 1} | acc = {dev_acc / len(dev_loader):.3f}")
+            model.train()
+
+    # Save a model and its configuration file to the directory 「saved_model」 
+    # i.e. there are two files under the direcory 「saved_model」: 「pytorch_model.bin」 and 「config.json」
+    # Saved model can be re-loaded using 「model = BertForQuestionAnswering.from_pretrained("saved_model")」
+    print("Saving Model ...")
+    model_save_dir = "saved_model" 
+    model.save_pretrained(model_save_dir)
 
 """## Testing"""
 
 print("Evaluating Test Set ...")
 
 result = []
-
+model = AutoModelForQuestionAnswering.from_pretrained("saved_model").to(device)
 model.eval()
 with torch.no_grad():
-    for data in tqdm(test_loader):
+    for i, data in enumerate(tqdm(test_loader)):
         output = model(input_ids=data[0].squeeze(dim=0).to(device), token_type_ids=data[1].squeeze(dim=0).to(device),
                        attention_mask=data[2].squeeze(dim=0).to(device))
-        result.append(evaluate(data, output))
+        result.append(evaluate(data, output, doc_stride=doc_stride, paragraph=test_paragraphs[test_questions[i]["paragraph_id"]],
+                               paragraph_tokenized=test_paragraphs_tokenized[test_questions[i]["paragraph_id"]]))
 
 result_file = "result.csv"
-with open(result_file, 'w') as f:	
+with open(result_file, 'w', encoding='utf-8-sig') as f:	
     f.write("ID,Answer\n")
     for i, test_question in enumerate(test_questions):
     # Replace commas in answers with empty strings (since csv is separated by comma)
@@ -402,107 +420,106 @@ with open(result_file, 'w') as f:
 
 print(f"Completed! Result is in {result_file}")
 
-"""# GradeScope - Question 2 (In-context learning)
+# """# GradeScope - Question 2 (In-context learning)
 
-### In-context learning
-The example prompt is :
-```
-請從最後一篇的文章中找出最後一個問題的答案：
-文章：<文章1 內容>
-問題：<問題1 敘述>
-答案：<答案1>
-...
-文章：<文章n 內容>
-問題：<問題n 敘述>
-答案：
-```
-"""
+# ### In-context learning
+# The example prompt is :
+# ```
+# 請從最後一篇的文章中找出最後一個問題的答案：
+# 文章：<文章1 內容>
+# 問題：<問題1 敘述>
+# 答案：<答案1>
+# ...
+# 文章：<文章n 內容>
+# 問題：<問題n 敘述>
+# 答案：
+# ```
+# """
 
-import torch
-import random  
-import numpy as np
+# import torch
+# import random  
+# import numpy as np
 
-# To avoid CUDA_OUT_OF_MEMORY
-torch.set_default_tensor_type(torch.cuda.FloatTensor)
+# # To avoid CUDA_OUT_OF_MEMORY
+# torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
-# Fix random seed for reproducibility
-def same_seeds(seed):
-	torch.manual_seed(seed)
-	if torch.cuda.is_available():
-			torch.cuda.manual_seed(seed)
-			torch.cuda.manual_seed_all(seed)
-	np.random.seed(seed)
-	random.seed(seed)
-	torch.backends.cudnn.benchmark = False
-	torch.backends.cudnn.deterministic = True
-same_seeds(2)
+# # Fix random seed for reproducibility
+# def same_seeds(seed):
+# 	torch.manual_seed(seed)
+# 	if torch.cuda.is_available():
+# 			torch.cuda.manual_seed(seed)
+# 			torch.cuda.manual_seed_all(seed)
+# 	np.random.seed(seed)
+# 	random.seed(seed)
+# 	torch.backends.cudnn.benchmark = False
+# 	torch.backends.cudnn.deterministic = True
+# same_seeds(2)
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# You can try model with different size
-# When using Colab or Kaggle, models with more than 2 billions parameters may 
-# run out of memory
-tokenizer = AutoTokenizer.from_pretrained("facebook/xglm-1.7B")
-model = AutoModelForCausalLM.from_pretrained("facebook/xglm-1.7B")
+# # You can try model with different size
+# # When using Colab or Kaggle, models with more than 2 billions parameters may 
+# # run out of memory
+# tokenizer = AutoTokenizer.from_pretrained("facebook/xglm-1.7B")
+# model = AutoModelForCausalLM.from_pretrained("facebook/xglm-1.7B")
 
-# To clean model output. If you try different prompts, you may have to fix 
-# this function on your own
-def clean_text(text):
-    # Note: When you use unilingual model, the colon may become fullwidth
-    text = text.split("答案:")[-1]
-    text = text.split(" ")[0]
-    return text
+# # To clean model output. If you try different prompts, you may have to fix 
+# # this function on your own
+# def clean_text(text):
+#     # Note: When you use unilingual model, the colon may become fullwidth
+#     text = text.split("答案:")[-1]
+#     text = text.split(" ")[0]
+#     return text
 
-import random
-import json
+# import random
+# import json
 
-with open("hw7_in-context-learning-examples.json", "r") as f: 
-    test = json.load(f)
+# with open("hw7_in-context-learning-examples.json", "r") as f: 
+#     test = json.load(f)
 
-# K-shot learning 
-# Give model K examples to make it achieve better accuracy 
-# Note: (1) When K >= 4, CUDA_OUT_OFF_MEMORY may occur.
-#       (2) The maximum input length of XGLM is 2048
-K = 2
+# # K-shot learning 
+# # Give model K examples to make it achieve better accuracy 
+# # Note: (1) When K >= 4, CUDA_OUT_OFF_MEMORY may occur.
+# #       (2) The maximum input length of XGLM is 2048
+# K = 2
 
-question_ids = [qa["id"] for qa in test["questions"]]
+# question_ids = [qa["id"] for qa in test["questions"]]
 
-with open("in-context-learning-result.txt", "w") as f:
-    print("ID,Ground-Truth,Prediction", file = f)
-    with torch.no_grad():
-        for idx, qa in enumerate(test["questions"]):
-            # You can try different prompts
-            prompt = "請從最後一篇的文章中找出最後一個問題的答案\n"
-            exist_question_indexs = [question_ids.index(qa["id"])]
+# with open("in-context-learning-result.txt", "w") as f:
+#     print("ID,Ground-Truth,Prediction", file = f)
+#     with torch.no_grad():
+#         for idx, qa in enumerate(test["questions"]):
+#             # You can try different prompts
+#             prompt = "請從最後一篇的文章中找出最後一個問題的答案\n"
+#             exist_question_indexs = [question_ids.index(qa["id"])]
 
-            # K-shot learning: give the model K examples with answers
-            for i in range(K):
-                question_index = question_ids.index(qa["id"])
-                while(question_index in exist_question_indexs): 
-                    question_index = random.randint(0, len(question_ids) - 1)
-                exist_question_indexs.append(question_index)    
-                paragraph_id = test["questions"][question_index]["paragraph_id"]
-                prompt += f'文章：{test["paragraphs"][paragraph_id]}\n'
-                prompt += f'問題：{test["questions"][question_index]["question_text"]}\n'
-                prompt += f'答案：{test["questions"][question_index]["answer_text"]}\n'
+#             # K-shot learning: give the model K examples with answers
+#             for i in range(K):
+#                 question_index = question_ids.index(qa["id"])
+#                 while(question_index in exist_question_indexs): 
+#                     question_index = random.randint(0, len(question_ids) - 1)
+#                 exist_question_indexs.append(question_index)    
+#                 paragraph_id = test["questions"][question_index]["paragraph_id"]
+#                 prompt += f'文章：{test["paragraphs"][paragraph_id]}\n'
+#                 prompt += f'問題：{test["questions"][question_index]["question_text"]}\n'
+#                 prompt += f'答案：{test["questions"][question_index]["answer_text"]}\n'
 
-            # The final one question without answer
-            paragraph_id = qa["paragraph_id"]
-            prompt += f'文章：{test["paragraphs"][paragraph_id]}\n'
-            prompt += f'問題：{qa["question_text"]}\n'
-            prompt += f'答案：'
+#             # The final one question without answer
+#             paragraph_id = qa["paragraph_id"]
+#             prompt += f'文章：{test["paragraphs"][paragraph_id]}\n'
+#             prompt += f'問題：{qa["question_text"]}\n'
+#             prompt += f'答案：'
             
-            inputs = tokenizer(prompt, add_special_tokens=False, return_tensors="pt") 
-            sample = model.generate(**inputs, max_new_tokens = 20)
-            text = tokenizer.decode(sample[0], skip_special_tokens=True)
+#             inputs = tokenizer(prompt, add_special_tokens=False, return_tensors="pt") 
+#             sample = model.generate(**inputs, max_new_tokens = 20)
+#             text = tokenizer.decode(sample[0], skip_special_tokens=True)
 
-            # Note: You can delete this line to see what will happen
-            text = clean_text(text)
+#             # Note: You can delete this line to see what will happen
+#             text = clean_text(text)
             
-            print(prompt)
-            print(f'正確答案: {qa["answer_text"]}')
-            print(f'模型輸出: {text}')
-            print()
+#             print(prompt)
+#             print(f'正確答案: {qa["answer_text"]}')
+#             print(f'模型輸出: {text}')
+#             print()
 
-            print(f"{idx},{qa['answer_text']},{text}", file = f)
-
+#             print(f"{idx},{qa['answer_text']},{text}", file = f)

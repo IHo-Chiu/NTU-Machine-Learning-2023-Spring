275370dd5677aaa0f60e0aeb71a953062c748464
IHo-Chiu <r11922189@ntu.edu.com>
2023-05-12 02:25:53 +0000

    .

--- HW7/main.py
+++ HW7/main.py
@@ -121,7 +121,7 @@ learning_rate = 5e-5
 train_batch_size = 8
 doc_stride = 32
 model_save_dir = "saved_model"
-ensemble_list = ["saved_model_1]
+ensemble_list = ["saved_model_1", ]
 
 #### TODO: gradient_accumulation (optional)####
 # Note: train_batch_size * gradient_accumulation_steps = effective batch size
